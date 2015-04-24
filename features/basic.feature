c23f124ea042a4ed2f439aaa7a7abe834ade0433
Ataru Kodaka <ataru.kodaka@gmail.com>
2015-04-24 14:44:52 +0000

    add helpers

--- features/basic.feature
+++ features/basic.feature
@@ -23,3 +23,23 @@ Feature: basic
     And the Server is running at "basic-app"
     When I go to "/index.html"
     Then I should see "test"
+
+  Scenario: option for regex
+    Given a fixture app "basic-app"
+    And a file named "config.rb" with:
+      """
+      activate :twitter_oembed do |twitter|
+        twitter.convert_regex = %r{\[\[twitter\:\s*(\d+)\]\]}
+      end
+      """
+    And a file named "source/index.html.erb" with:
+      """
+      ---
+      ---
+      [[twitter: 591598644421242880]]
+      """
+    And the Server is running at "basic-app"
+    When I go to "/index.html"
+    Then I should see "test"
+    
+
--- lib/middleman-twitter-oembed/extension.rb
+++ lib/middleman-twitter-oembed/extension.rb
@@ -1,28 +1,19 @@
+require 'middleman-twitter-oembed/helpers'
+module Middleman::TwitterOembed
+  class Interface
+    def initialize(options)
+    end
+  end
+end
+
 module Middleman
   module TwitterOembed
-    module Helpers
-      def twitter_oembed(status_id)
-        ext = extensions[:twitter_oembed]
-        res = ext.get_tweet(status_id)
-        res['html']
-      end
-      alias_method :twitter_oembed_by_status_id, :twitter_oembed
-      
-=begin
-      def twitter_oembed_by_url(url)
-        raise
-        regex = %r{https?://twitter.com/[a-zA-Z0-9_]+/status/(\d+)}
-        url =~ regex
-        status_id = $1
-        logger.debug url
-        logger.debug status_id
-        twitter_oembed(status_id)
-      end
-=end
-    end ## module Helpers
-    
     class Extension < Middleman::Extension
       include ERB::Util
+      
+      option :convert_regex, %r{https?://twitter.com/[a-zA-Z0-9_]+/status/(\d+)}
+      option :use_cache, true
+      option :cache_dir, ".caches/twitter_oembed"
       
       self.defined_helpers = [::Middleman::TwitterOembed::Helpers]
 
@@ -30,6 +21,8 @@ module Middleman
         super
         #klass.set :twitter_oembed_settings, options
 
+        @result_cache = {}
+        
         extension = self
         klass.before_render do |body|
           extension.convert(body)
@@ -39,15 +32,40 @@ module Middleman
         require 'open-uri'
         require 'json'
 
-        regex = %r{https?://twitter.com/[a-zA-Z0-9_]+/status/(\d+)}
+        #regex = %r{https?://twitter.com/[a-zA-Z0-9_]+/status/(\d+)}
+        regex = options.convert_regex
         body.gsub(regex){
           res = get_tweet($1)
           res['html']
         }
       end
       def get_tweet(status_id)
+        return @result_cache[status_id] if @result_cache.has_key?(status_id)
+        return @result_cache[status_id] = load_cache(status_id) if options.use_cache && File.exist?(cache_filename(status_id))
+        
         url = "https://api.twitter.com/1/statuses/oembed.json?id=#{h(status_id)}"
-        JSON.parse(open(url).read)
+        begin
+          res = JSON.parse(open(url).read)
+          ## yet to rescue
+        end
+
+        @result_cache[status_id] = res
+        save_cache(status_id, res) if options.use_cache
+
+        return res
+      end
+      private
+      def cache_filename(status_id)
+        File.join(options.cache_dir, status_id)
+      end
+      def load_cache(asin)
+        Marshal.load(File.read(cache_filename(asin)))
+      end
+      def save_cache(asin, hash)
+        dir = File.dirname(cache_filename(asin))
+        require 'fileutils'
+        FileUtils.mkdir_p(dir) unless File.exists?(dir)
+        File.open(cache_filename(asin), 'wb'){|f| f.write(Marshal.dump(hash))}
       end
     end ## class Extension
   end
